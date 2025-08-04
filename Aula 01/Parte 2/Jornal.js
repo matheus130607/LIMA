0436709558b083888ecff8990b83ab8c4c3cdf1c
matheus130607 <matheusmalaman2014@gmail.com>
2025-08-04 15:00:51 -0300

    Atualizado

--- Aula 01/Parte 2/Jornal.js
+++ Aula 01/Parte 2/Jornal.js
@@ -1,7 +1,9 @@
-function mostrarMensagem(id) {
-    const mensagens = ['msg1', 'msg2', 'msg3', 'msg4'];
+function sobre(sesao) {
+    document.getElementById('msg1').classList.add("hidden");
+    document.getElementById('msg2').classList.add("hidden");
+    document.getElementById('msg3').classList.add("hidden");
+    document.getElementById('msg4').classList.add("hidden");
 
-    mensagens.forEach(function(mensagemId) {
-        document.getElementById(mensagemId).hidden = (mensagemId !== id);
-    });
+    document.getElementById(sesao).classList.remove("hidden");
 }
+
