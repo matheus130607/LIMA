a1ef1d5c94b39771640ee668921b6c458644b76f
matheus130607 <matheusmalaman2014@gmail.com>
2025-08-14 16:53:52 -0300

    14/08/2025

---
+++ Atividades/Aula 02/inicial.js
@@ -0,0 +1,15 @@
+function cadastro() {
+    window.location.href = "cadastro.html"
+}
+
+function eventos() {
+    window.location.href = "eventos.html"
+}
+
+function faleconosco() {
+    window.location.href = "faleconosco.html"
+}
+
+function inicio() {
+    window.location.href = "Inicial.html"
+}
